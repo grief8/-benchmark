7c5a3d66df1ce5ca3be261d8fd0687c63f837bf1
github-action-benchmark <github@users.noreply.github.com>
2024-07-10 20:04:40 +0000

    add lmbench-signal Benchmark (customSmallerIsBetter) benchmark result for 9e5f3123e1f980d82b50757dc358e6ff8344d14c

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1720641744890,
+  "lastUpdate": 1720641880702,
   "repoUrl": "https://github.com/asterinas/asterinas",
   "entries": {
     "sysbench-thread Benchmark": [
@@ -1606,6 +1606,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Average Signal handler overhead on Asterinas",
             "value": "0.9632",
+            "unit": "ms",
+            "extra": "aster_avg"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Zhang Junyang",
+            "username": "junyang-zh",
+            "email": "junyang@stu.pku.edu.cn"
+          },
+          "committer": {
+            "name": "Tate, Hongliang Tian",
+            "username": "tatetian",
+            "email": "tatetian@gmail.com"
+          },
+          "id": "9e5f3123e1f980d82b50757dc358e6ff8344d14c",
+          "message": "Log messages to the serial atomically and rename `arch::serial`\n\nCo-authored-by: Chuandong Li <lichuand@pku.edu.cn>",
+          "timestamp": "2024-06-29T11:54:01Z",
+          "url": "https://github.com/asterinas/asterinas/commit/9e5f3123e1f980d82b50757dc358e6ff8344d14c"
+        },
+        "date": 1720641877586,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Average Signal handler overhead on Linux",
+            "value": "1.0682",
+            "unit": "ms",
+            "extra": "linux_avg"
+          },
+          {
+            "name": "Average Signal handler overhead on Asterinas",
+            "value": "0.9542",
             "unit": "ms",
             "extra": "aster_avg"
           }
