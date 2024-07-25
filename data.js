e25cf2d34e02615eb1b9b1a3ac54f3a825b4c04c
github-action-benchmark <github@users.noreply.github.com>
2024-07-25 20:38:40 +0000

    add lmbench-stat Benchmark (customSmallerIsBetter) benchmark result for 0b8335c9431008dd7d037721a21724328c4a3024

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1721939801067,
+  "lastUpdate": 1721939920402,
   "repoUrl": "https://github.com/grief8/asterinas",
   "entries": {
     "sysbench-thread Benchmark": [
@@ -6951,6 +6951,39 @@ window.BENCHMARK_DATA = {
           {
             "name": "Average stat latency on Asterinas",
             "value": "1.9436",
+            "unit": "ms",
+            "extra": "aster_avg"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "StanPlatinum",
+            "email": "yangzhi.lwj@antgroup.com"
+          },
+          "committer": {
+            "name": "Tate, Hongliang Tian",
+            "username": "tatetian",
+            "email": "tatetian@gmail.com"
+          },
+          "id": "0b8335c9431008dd7d037721a21724328c4a3024",
+          "message": "Add /proc/filesystems support",
+          "timestamp": "2024-07-13T09:31:50Z",
+          "url": "https://github.com/grief8/asterinas/commit/0b8335c9431008dd7d037721a21724328c4a3024"
+        },
+        "date": 1721939916911,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Average stat latency on Linux",
+            "value": "0.5615",
+            "unit": "ms",
+            "extra": "linux_avg"
+          },
+          {
+            "name": "Average stat latency on Asterinas",
+            "value": "1.9188",
             "unit": "ms",
             "extra": "aster_avg"
           }
