f6f3a439719ce5e53c417ebd4103b5efbd2a998b
github-action-benchmark <github@users.noreply.github.com>
2024-07-26 20:08:06 +0000

    add lmbench-fstat Benchmark (customSmallerIsBetter) benchmark result for e83e1fc01ba38ad2a405d7d710ec7258fb664f60

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1722024366172,
+  "lastUpdate": 1722024486878,
   "repoUrl": "https://github.com/asterinas/asterinas",
   "entries": {
     "sysbench-thread Benchmark": [
@@ -5310,6 +5310,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Average fstat latency on Asterinas",
             "value": "0.9301",
+            "unit": "ms",
+            "extra": "aster_avg"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Ruihan Li",
+            "username": "lrh2000",
+            "email": "lrh2000@pku.edu.cn"
+          },
+          "committer": {
+            "name": "Tate, Hongliang Tian",
+            "username": "tatetian",
+            "email": "tatetian@gmail.com"
+          },
+          "id": "e83e1fc01ba38ad2a405d7d710ec7258fb664f60",
+          "message": "Unpack states from `Arc` in UNIX sockets",
+          "timestamp": "2024-06-30T15:08:25Z",
+          "url": "https://github.com/asterinas/asterinas/commit/e83e1fc01ba38ad2a405d7d710ec7258fb664f60"
+        },
+        "date": 1722024485046,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Average fstat latency on Linux",
+            "value": "0.5176",
+            "unit": "ms",
+            "extra": "linux_avg"
+          },
+          {
+            "name": "Average fstat latency on Asterinas",
+            "value": "0.9460",
             "unit": "ms",
             "extra": "aster_avg"
           }
