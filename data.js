6b451ed146cf82fa3b4e63f481bcfaaadfc3deda
github-action-benchmark <github@users.noreply.github.com>
2024-07-31 20:22:34 +0000

    add lmbench-pipe-latency Benchmark (customSmallerIsBetter) benchmark result for 20a856b07fa8210fdd2d46d3feb5087004c27afb

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1722457211967,
+  "lastUpdate": 1722457354973,
   "repoUrl": "https://github.com/asterinas/asterinas",
   "entries": {
     "sysbench-thread Benchmark": [
@@ -9605,6 +9605,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Average pipe latency on Asterinas",
             "value": "5.4062",
+            "unit": "ms",
+            "extra": "aster_avg"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Shaowei Song",
+            "username": "lucassong-mh",
+            "email": "songshaowei.ssw@antgroup.com"
+          },
+          "committer": {
+            "name": "Tate, Hongliang Tian",
+            "username": "tatetian",
+            "email": "tatetian@gmail.com"
+          },
+          "id": "20a856b07fa8210fdd2d46d3feb5087004c27afb",
+          "message": "Revise the naming of all sync/async io APIs",
+          "timestamp": "2024-07-30T09:31:02Z",
+          "url": "https://github.com/asterinas/asterinas/commit/20a856b07fa8210fdd2d46d3feb5087004c27afb"
+        },
+        "date": 1722457353202,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Average pipe latency on Linux",
+            "value": "3.7721",
+            "unit": "ms",
+            "extra": "linux_avg"
+          },
+          {
+            "name": "Average pipe latency on Asterinas",
+            "value": "5.3951",
             "unit": "ms",
             "extra": "aster_avg"
           }
