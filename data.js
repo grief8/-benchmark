aef9cf8e232a6b8cb0c70e5b13f9bcfabaad711e
github-action-benchmark <github@users.noreply.github.com>
2024-08-28 20:09:25 +0000

    add lmbench-fifo-latency Benchmark (customSmallerIsBetter) benchmark result for e6f465b553f613d60828e0e503dc94ce4109ded1

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1724875613528,
+  "lastUpdate": 1724875765897,
   "repoUrl": "https://github.com/asterinas/asterinas",
   "entries": {
     "sysbench-thread Benchmark": [
@@ -35378,6 +35378,41 @@ window.BENCHMARK_DATA = {
           {
             "name": "Average fifo latency on Asterinas",
             "value": "3.7309",
+            "unit": "µs",
+            "extra": "aster_avg"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Shaowei Song",
+            "username": "lucassong-mh",
+            "email": "songshaowei.ssw@antgroup.com"
+          },
+          "committer": {
+            "name": "Tate, Hongliang Tian",
+            "username": "tatetian",
+            "email": "tatetian@gmail.com"
+          },
+          "id": "e6f465b553f613d60828e0e503dc94ce4109ded1",
+          "message": "Replace all the ring buffers with the new one",
+          "timestamp": "2024-08-28T06:23:24Z",
+          "url": "https://github.com/asterinas/asterinas/commit/e6f465b553f613d60828e0e503dc94ce4109ded1"
+        },
+        "date": 1724875761899,
+        "tool": "customSmallerIsBetter",
+        "description": "The latency of fifo on a single processor.",
+        "benches": [
+          {
+            "name": "Average fifo latency on Linux",
+            "value": "2.6002",
+            "unit": "µs",
+            "extra": "linux_avg"
+          },
+          {
+            "name": "Average fifo latency on Asterinas",
+            "value": "3.7057",
             "unit": "µs",
             "extra": "aster_avg"
           }
