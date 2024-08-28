18d24d9270dcb8b0e7465210fe3391bd1c8b67fb
github-action-benchmark <github@users.noreply.github.com>
2024-08-28 04:00:58 +0000

    add lmbench-ctx Benchmark (customSmallerIsBetter) benchmark result for ffc717f00bce62b1805af8043e335ad5fc1f175c

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1724707804876,
+  "lastUpdate": 1724817658048,
   "repoUrl": "https://github.com/asterinas/asterinas",
   "entries": {
     "sysbench-thread Benchmark": [
@@ -10872,6 +10872,41 @@ window.BENCHMARK_DATA = {
           {
             "name": "Average context switch latency on Asterinas",
             "value": "1.85",
+            "unit": "µs",
+            "extra": "aster_avg"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Chen Chengjun",
+            "username": "cchanging",
+            "email": "chenchengjun.ccj@antgroup.com"
+          },
+          "committer": {
+            "name": "Tate, Hongliang Tian",
+            "username": "tatetian",
+            "email": "tatetian@gmail.com"
+          },
+          "id": "ffc717f00bce62b1805af8043e335ad5fc1f175c",
+          "message": "Enable handling page fault around",
+          "timestamp": "2024-08-28T02:24:44Z",
+          "url": "https://github.com/asterinas/asterinas/commit/ffc717f00bce62b1805af8043e335ad5fc1f175c"
+        },
+        "date": 1724817654683,
+        "tool": "customSmallerIsBetter",
+        "description": "The latency of context switching between 18 contexts on a single processor.",
+        "benches": [
+          {
+            "name": "Average context switch latency on Linux",
+            "value": "1.06",
+            "unit": "µs",
+            "extra": "linux_avg"
+          },
+          {
+            "name": "Average context switch latency on Asterinas",
+            "value": "1.83",
             "unit": "µs",
             "extra": "aster_avg"
           }
