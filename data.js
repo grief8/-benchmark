a47c4bdc1e0188fe1aaea555fd336be282c7dc69
github-action-benchmark <github@users.noreply.github.com>
2024-09-01 20:12:29 +0000

    add lmbench-fifo-latency Benchmark (customSmallerIsBetter) benchmark result for 745ac6d982638d3548f09ea2aa2103e37c135153

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1725221370227,
+  "lastUpdate": 1725221549467,
   "repoUrl": "https://github.com/asterinas/asterinas",
   "entries": {
     "sysbench-thread Benchmark": [
@@ -42308,6 +42308,41 @@ window.BENCHMARK_DATA = {
           {
             "name": "Average fifo latency on Asterinas",
             "value": "3.6549",
+            "unit": "µs",
+            "extra": "aster_avg"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Marsman1996",
+            "username": "Marsman1996",
+            "email": "lqliuyuwei@outlook.com"
+          },
+          "committer": {
+            "name": "Tate, Hongliang Tian",
+            "username": "tatetian",
+            "email": "tatetian@gmail.com"
+          },
+          "id": "745ac6d982638d3548f09ea2aa2103e37c135153",
+          "message": "Set overflow boundary to `isize::MAX` for memory related syscalls",
+          "timestamp": "2024-08-29T14:26:36Z",
+          "url": "https://github.com/asterinas/asterinas/commit/745ac6d982638d3548f09ea2aa2103e37c135153"
+        },
+        "date": 1725221545380,
+        "tool": "customSmallerIsBetter",
+        "description": "The latency of fifo on a single processor.",
+        "benches": [
+          {
+            "name": "Average fifo latency on Linux",
+            "value": "2.6373",
+            "unit": "µs",
+            "extra": "linux_avg"
+          },
+          {
+            "name": "Average fifo latency on Asterinas",
+            "value": "3.6591",
             "unit": "µs",
             "extra": "aster_avg"
           }
