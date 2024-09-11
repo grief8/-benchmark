3938faa97720d807d31b2ffb993e2f9598c4d295
github-action-benchmark <github@users.noreply.github.com>
2024-09-11 20:10:00 +0000

    add lmbench-fifo-latency Benchmark (customSmallerIsBetter) benchmark result for 8bfbdf6642a9d04db3490792926d6a299d1e30be

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1726085223756,
+  "lastUpdate": 1726085400330,
   "repoUrl": "https://github.com/asterinas/asterinas",
   "entries": {
     "sysbench-thread Benchmark": [
@@ -53508,6 +53508,41 @@ window.BENCHMARK_DATA = {
           {
             "name": "Average fifo latency on Asterinas",
             "value": "3.6591",
+            "unit": "µs",
+            "extra": "aster_avg"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Shaowei Song",
+            "username": "lucassong-mh",
+            "email": "songshaowei.ssw@antgroup.com"
+          },
+          "committer": {
+            "name": "Tate, Hongliang Tian",
+            "username": "tatetian",
+            "email": "tatetian@gmail.com"
+          },
+          "id": "8bfbdf6642a9d04db3490792926d6a299d1e30be",
+          "message": "Optimize the lock usage in `RamInode`'s read/write",
+          "timestamp": "2024-09-02T13:23:46Z",
+          "url": "https://github.com/asterinas/asterinas/commit/8bfbdf6642a9d04db3490792926d6a299d1e30be"
+        },
+        "date": 1726085394921,
+        "tool": "customSmallerIsBetter",
+        "description": "The latency of fifo on a single processor.",
+        "benches": [
+          {
+            "name": "Average fifo latency on Linux",
+            "value": "2.6357",
+            "unit": "µs",
+            "extra": "linux_avg"
+          },
+          {
+            "name": "Average fifo latency on Asterinas",
+            "value": "3.3601",
             "unit": "µs",
             "extra": "aster_avg"
           }
