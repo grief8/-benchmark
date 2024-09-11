b9c425278f1e9b83144e182cfeb2410e2b27d291
github-action-benchmark <github@users.noreply.github.com>
2024-09-11 21:20:53 +0000

    add lmbench-select-tcp Benchmark (customSmallerIsBetter) benchmark result for 64ce6334edcd7bd4c2ecac0236c27513285bb50e

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1726089486443,
+  "lastUpdate": 1726089653451,
   "repoUrl": "https://github.com/grief8/asterinas",
   "entries": {
     "sysbench-thread Benchmark": [
@@ -61494,6 +61494,41 @@ window.BENCHMARK_DATA = {
           {
             "name": "Average select TCP latency on Asterinas",
             "value": "1.6566",
+            "unit": "µs",
+            "extra": "aster_avg"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Fabing Li",
+            "username": "grief8",
+            "email": "lifabing.lfb@antgroup.com"
+          },
+          "committer": {
+            "name": "Fabing Li",
+            "username": "grief8",
+            "email": "lifabing.lfb@antgroup.com"
+          },
+          "id": "64ce6334edcd7bd4c2ecac0236c27513285bb50e",
+          "message": "Jiang patch",
+          "timestamp": "2024-09-04T11:23:09Z",
+          "url": "https://github.com/grief8/asterinas/commit/64ce6334edcd7bd4c2ecac0236c27513285bb50e"
+        },
+        "date": 1726089650766,
+        "tool": "customSmallerIsBetter",
+        "description": "The latency of select TCP on a single processor.",
+        "benches": [
+          {
+            "name": "Average select TCP latency on Linux",
+            "value": "2.8540",
+            "unit": "µs",
+            "extra": "linux_avg"
+          },
+          {
+            "name": "Average select TCP latency on Asterinas",
+            "value": "1.6359",
             "unit": "µs",
             "extra": "aster_avg"
           }
