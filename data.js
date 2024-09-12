04eb14327f91dae4b97d9a8ab9a317fa8c36076e
github-action-benchmark <github@users.noreply.github.com>
2024-09-12 20:58:57 +0000

    add lmbench-select-tcp Benchmark (customSmallerIsBetter) benchmark result for 6fcad6ce143b8f65e8be76297eb95a9cb56da22e

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1726174599506,
+  "lastUpdate": 1726174737289,
   "repoUrl": "https://github.com/asterinas/asterinas",
   "entries": {
     "sysbench-thread Benchmark": [
@@ -57049,6 +57049,41 @@ window.BENCHMARK_DATA = {
           {
             "name": "Average select TCP latency on Asterinas",
             "value": "1.6771",
+            "unit": "µs",
+            "extra": "aster_avg"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Marsman1996",
+            "username": "Marsman1996",
+            "email": "lqliuyuwei@outlook.com"
+          },
+          "committer": {
+            "name": "Tate, Hongliang Tian",
+            "username": "tatetian",
+            "email": "tatetian@gmail.com"
+          },
+          "id": "6fcad6ce143b8f65e8be76297eb95a9cb56da22e",
+          "message": "Fix integer overflow in handle_page_faults_around",
+          "timestamp": "2024-09-12T12:15:04Z",
+          "url": "https://github.com/asterinas/asterinas/commit/6fcad6ce143b8f65e8be76297eb95a9cb56da22e"
+        },
+        "date": 1726174733827,
+        "tool": "customSmallerIsBetter",
+        "description": "The latency of select TCP on a single processor.",
+        "benches": [
+          {
+            "name": "Average select TCP latency on Linux",
+            "value": "2.8351",
+            "unit": "µs",
+            "extra": "linux_avg"
+          },
+          {
+            "name": "Average select TCP latency on Asterinas",
+            "value": "1.6729",
             "unit": "µs",
             "extra": "aster_avg"
           }
