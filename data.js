56df489f6c75108fa1aebba5d70ea931e02f8039
github-action-benchmark <github@users.noreply.github.com>
2024-09-13 14:18:43 +0000

    add lmbench/tcp_loopback_lat (customSmallerIsBetter) benchmark result for 2426c1687d205c276ff6a4af1ad3a9f92ae74355

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1726237116960,
+  "lastUpdate": 1726237123435,
   "repoUrl": "https://github.com/grief8/asterinas",
   "entries": {
     "sysbench/thread_lat": [
@@ -59947,6 +59947,45 @@ window.BENCHMARK_DATA = {
             "value": "372.9157",
             "unit": "µs",
             "extra": "aster_avg"
+          }
+        ]
+      }
+    ],
+    "lmbench/tcp_loopback_lat": [
+      {
+        "commit": {
+          "author": {
+            "name": "Fabing Li",
+            "username": "grief8",
+            "email": "lifabing.lfb@antgroup.com"
+          },
+          "committer": {
+            "name": "Fabing Li",
+            "username": "grief8",
+            "email": "lifabing.lfb@antgroup.com"
+          },
+          "id": "2426c1687d205c276ff6a4af1ad3a9f92ae74355",
+          "message": "ss",
+          "timestamp": "2024-09-13T10:48:35Z",
+          "url": "https://github.com/grief8/asterinas/commit/2426c1687d205c276ff6a4af1ad3a9f92ae74355"
+        },
+        "date": 1726237108345,
+        "tool": "customSmallerIsBetter",
+        "title": "[TCP sockets] The latency of write+read",
+        "description": "lat_tcp",
+        "display": true,
+        "benches": [
+          {
+            "name": "Average TCP latency on Linux",
+            "value": "6.2124",
+            "unit": "µs",
+            "extra": "linux_result"
+          },
+          {
+            "name": "Average TCP latency on Asterinas",
+            "value": "12.8806",
+            "unit": "µs",
+            "extra": "aster_result"
           }
         ]
       }
