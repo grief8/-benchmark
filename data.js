67a3e12fda3a8ac1f898fa821ef7609382785cca
github-action-benchmark <github@users.noreply.github.com>
2024-09-14 03:54:04 +0000

    add iperf3/tcp_bw (customSmallerIsBetter) benchmark result for adfb4a081356bea739a1720f284c3e66d6a96d19

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1726286038907,
+  "lastUpdate": 1726286044552,
   "repoUrl": "https://github.com/grief8/asterinas",
   "entries": {
     "sysbench/thread_lat": [
@@ -61576,6 +61576,45 @@ window.BENCHMARK_DATA = {
             "name": "Average TCP latency on Asterinas",
             "value": "15.4622",
             "unit": "µs",
+            "extra": "aster_result"
+          }
+        ]
+      }
+    ],
+    "iperf3/tcp_bw": [
+      {
+        "commit": {
+          "author": {
+            "name": "Fabing Li",
+            "username": "grief8",
+            "email": "lifabing.lfb@antgroup.com"
+          },
+          "committer": {
+            "name": "Fabing Li",
+            "username": "grief8",
+            "email": "lifabing.lfb@antgroup.com"
+          },
+          "id": "adfb4a081356bea739a1720f284c3e66d6a96d19",
+          "message": "test",
+          "timestamp": "2024-09-14T03:47:04Z",
+          "url": "https://github.com/grief8/asterinas/commit/adfb4a081356bea739a1720f284c3e66d6a96d19"
+        },
+        "date": 1726286025826,
+        "tool": "customSmallerIsBetter",
+        "title": "[Network] iperf3 sender performance",
+        "description": "iperf3 -s -B 10.0.2.15",
+        "display": true,
+        "benches": [
+          {
+            "name": "Average Bandwidth between Host Linux and Guest Linux",
+            "value": "5424",
+            "unit": "Mbits/sec",
+            "extra": "linux_result"
+          },
+          {
+            "name": "Average Bandwidth between Host Linux and Guest Asterinas",
+            "value": "8.28",
+            "unit": "Mbits/sec",
             "extra": "aster_result"
           }
         ]
