d9663ab10292298325c4d4f37b8dee649f89fd7b
github-action-benchmark <github@users.noreply.github.com>
2024-11-08 09:39:29 +0000

    add mem_mmap_lat (customSmallerIsBetter) benchmark result for af23a192ecc9ef7858cfef17df5ca706a42f44c6

---
+++ lmbench/data.js
@@ -0,0 +1,45 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1731058769355,
+  "repoUrl": "https://github.com/grief8/asterinas",
+  "entries": {
+    "mem_mmap_lat": [
+      {
+        "commit": {
+          "author": {
+            "name": "Fabing Li",
+            "username": "grief8",
+            "email": "lifabing.lfb@antgroup.com"
+          },
+          "committer": {
+            "name": "Fabing Li",
+            "username": "grief8",
+            "email": "lifabing.lfb@antgroup.com"
+          },
+          "id": "af23a192ecc9ef7858cfef17df5ca706a42f44c6",
+          "message": "tt",
+          "timestamp": "2024-11-08T09:31:51Z",
+          "url": "https://github.com/grief8/asterinas/commit/af23a192ecc9ef7858cfef17df5ca706a42f44c6"
+        },
+        "date": 1731058763202,
+        "tool": "customSmallerIsBetter",
+        "title": "[Memory] The cost of mmap+unmap",
+        "description": "lat_mmap",
+        "display": true,
+        "benches": [
+          {
+            "name": "Average mmap latency on Linux",
+            "value": "20",
+            "unit": "µs",
+            "extra": "linux_result"
+          },
+          {
+            "name": "Average mmap latency on Asterinas",
+            "value": "19",
+            "unit": "µs",
+            "extra": "aster_result"
+          }
+        ]
+      }
+    ]
+  }
+}
