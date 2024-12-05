2bfdc379e57843fd82064801ab992adf203ec0ac
github-action-benchmark <github@users.noreply.github.com>
2024-12-05 03:26:45 +0000

    add ext2_deletes_individual (customSmallerIsBetter) benchmark result for f7477c3d2ad65bf54ae449b65c37bfbebc8ac116

--- sqlite/data.js
+++ sqlite/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1733369189749,
+  "lastUpdate": 1733369205946,
   "repoUrl": "https://github.com/grief8/asterinas",
   "entries": {
     "ext2_deletes_between": [
@@ -703,6 +703,43 @@ window.BENCHMARK_DATA = {
           {
             "name": "The speed of deletes-individual on Asterinas (Ext2)",
             "value": "0.153",
+            "unit": "second",
+            "extra": "aster_result"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Fabing Li",
+            "username": "grief8",
+            "email": "lifabing.lfb@antgroup.com"
+          },
+          "committer": {
+            "name": "Fabing Li",
+            "username": "grief8",
+            "email": "lifabing.lfb@antgroup.com"
+          },
+          "id": "f7477c3d2ad65bf54ae449b65c37bfbebc8ac116",
+          "message": "Add",
+          "timestamp": "2024-12-05T03:17:38Z",
+          "url": "https://github.com/grief8/asterinas/commit/f7477c3d2ad65bf54ae449b65c37bfbebc8ac116"
+        },
+        "date": 1733369199861,
+        "tool": "customSmallerIsBetter",
+        "title": "[Ext2] The deletes-individual result of sqlite speedtest",
+        "description": "sqlite-speed-test: 50000 DELETEs of individual rows",
+        "display": false,
+        "benches": [
+          {
+            "name": "The speed of deletes-individual on Linux (Ext2)",
+            "value": "0.129",
+            "unit": "second",
+            "extra": "linux_result"
+          },
+          {
+            "name": "The speed of deletes-individual on Asterinas (Ext2)",
+            "value": "0.161",
             "unit": "second",
             "extra": "aster_result"
           }
