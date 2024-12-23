6909c5b3eef4183c81ae5d60af6c43c1c1dfe0cf
github-action-benchmark <github@users.noreply.github.com>
2024-12-23 07:23:00 +0000

    add p99_wakeup_latency_smp1 (customSmallerIsBetter) benchmark result for d6a07c28c389660e91bf2d46852033ce257a2f7b

--- schbench/data.js
+++ schbench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1734938565151,
+  "lastUpdate": 1734938580388,
   "repoUrl": "https://github.com/grief8/asterinas",
   "entries": {
     "p50_rps_smp1": [
@@ -74,6 +74,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "P99 request latency of schbench on Asterinas",
             "value": "5256",
+            "unit": "µs",
+            "extra": "aster_result"
+          }
+        ]
+      }
+    ],
+    "p99_wakeup_latency_smp1": [
+      {
+        "commit": {
+          "author": {
+            "name": "Fabing Li",
+            "username": "grief8",
+            "email": "lifabing.lfb@antgroup.com"
+          },
+          "committer": {
+            "name": "Fabing Li",
+            "username": "grief8",
+            "email": "lifabing.lfb@antgroup.com"
+          },
+          "id": "d6a07c28c389660e91bf2d46852033ce257a2f7b",
+          "message": "e",
+          "timestamp": "2024-12-23T07:12:49Z",
+          "url": "https://github.com/grief8/asterinas/commit/d6a07c28c389660e91bf2d46852033ce257a2f7b"
+        },
+        "date": 1734938575098,
+        "tool": "customSmallerIsBetter",
+        "title": "[Scheduler] P99 wakeup latency while max-loading the only CPU",
+        "description": "schbench -F 256 -n 5 -r 90",
+        "display": false,
+        "benches": [
+          {
+            "name": "P99 wakeup latency of schbench on Linux",
+            "value": "2",
+            "unit": "µs",
+            "extra": "linux_result"
+          },
+          {
+            "name": "P99 wakeup latency of schbench on Asterinas",
+            "value": "1",
             "unit": "µs",
             "extra": "aster_result"
           }
